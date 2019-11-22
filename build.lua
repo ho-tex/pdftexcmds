8f28668bd9cac36695165dfd65825ffbcb695914
David Carlisle <d.p.carlisle@gmail.com>
2019-11-22 18:54:15 +0000

    start at l3build setup

---
+++ build.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env texlua
+
+
+module = "pdftexcmds"
+
+textfiles = {"README.md"}
+unpackfiles = {'pdftexcmds.dtx'}
+sourcefiles={"*.dtx"}
+
+maxprintline=10000
+checkruns = 2
+
+
+
+
+
